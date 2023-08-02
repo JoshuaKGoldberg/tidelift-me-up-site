73c3622924c70de8d8c962f6ca8086509f9c95b5
rachel <jrachelr1@gmail.com>
2023-08-02 12:58:10 -0700

    feat/scroll-to-top: console logs are pulling the correct elements but scrollTo still not working.

--- src/components/ScrollButton.tsx
+++ src/components/ScrollButton.tsx
@@ -3,22 +3,25 @@
 import Link from "next/link";
 
 export function ScrollButton() {
-	// const scrollToTop = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
-	// 	e.preventDefault();
-	// 	const href = e.currentTarget.href;
-	// 	const targetID = href.replace(/.*#/, "");
-	// 	const elem = document.getElementById(targetID);
-	// 	window.scrollTo({
-	// 		top: elem?.getBoundingClientRect().top,
-	// 		// eslint-disable-next-line perfectionist/sort-objects
-	// 		behavior: "smooth",
-	// 	});
-	// };
+	const scrollToTop = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+		e.preventDefault();
+		const href = e.currentTarget.href;
+		console.log("href:", href);
+		const targetID = href.replace(/.*#/, "");
+		console.log("targetID:", targetID);
+		const elem = document.getElementById(targetID);
+		console.log("elem:", elem);
+		window.scrollTo({
+			top: elem?.getBoundingClientRect().top,
+			// eslint-disable-next-line perfectionist/sort-objects
+			behavior: "smooth",
+		});
+	};
 
 	return (
 		<Link
 			href="#title"
-			// onClick={scrollToTop}
+			onClick={scrollToTop}
 			type="button"
 			// style={{ display: visible ? "inline" : "none" }}
 		>
