04f98c1745bf43a6b53a82a93d2f5c69eda3fd15
rachel <jrachelr1@gmail.com>
2023-08-03 17:34:47 -0700

    feat/scroll-to-top: button working. removed Link and changed scrollTo to top of body

--- src/components/ScrollButton.tsx
+++ src/components/ScrollButton.tsx
@@ -1,31 +1,16 @@
 "use client";
 
-import Link from "next/link";
-
 export function ScrollButton() {
-	const scrollToTop = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
-		e.preventDefault();
-		const href = e.currentTarget.href;
-		console.log("href:", href);
-		const targetID = href.replace(/.*#/, "");
-		console.log("targetID:", targetID);
-		const elem = document.getElementById(targetID);
-		console.log("elem:", elem);
-		window.scrollTo({
-			top: elem?.getBoundingClientRect().top,
-			// eslint-disable-next-line perfectionist/sort-objects
+	const scrollToTop = () => {
+		document.body.scrollTo({
 			behavior: "smooth",
+			top: 0,
 		});
 	};
 
 	return (
-		<Link
-			href="#title"
-			onClick={scrollToTop}
-			type="button"
-			// style={{ display: visible ? "inline" : "none" }}
-		>
+		<button onClick={scrollToTop} type="button">
 			Back to Top⬆
-		</Link>
+		</button>
 	);
 }
