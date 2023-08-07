e64c7d258125712ee92ab4b4db00a7b3e64a6b06
rachel <jrachelr1@gmail.com>
2023-08-07 16:33:54 -0700

    feat/scroll-to-top: adding logic to only show button after scrolling, scrollTop currently returning 0.

--- src/components/ScrollButton.tsx
+++ src/components/ScrollButton.tsx
@@ -1,6 +1,21 @@
 "use client";
 
+import { useState } from "react";
+
 export function ScrollButton() {
+	const [visible, setVisible] = useState(false);
+
+	const toggleVisible = () => {
+		const scrolled = document.documentElement.scrollTop;
+		if (scrolled > 100) {
+			setVisible(true);
+			console.log("we be scrolling", scrolled);
+		} else if (scrolled <= 100) {
+			setVisible(false);
+			console.log("no scroll", scrolled);
+		}
+	};
+
 	const scrollToTop = () => {
 		document.body.scrollTo({
 			behavior: "smooth",
@@ -8,8 +23,14 @@ export function ScrollButton() {
 		});
 	};
 
+	document.body.addEventListener("scroll", toggleVisible);
+
 	return (
-		<button onClick={scrollToTop} type="button">
+		<button
+			onClick={scrollToTop}
+			style={{ display: visible ? "inline" : "none" }}
+			type="button"
+		>
 			Back to Top⬆
 		</button>
 	);
